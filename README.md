a427d3cb9dcc5e22ffa9665daeadf04627e30e07
Martin Erik Rieland Bindslev Bisgaard <mebisgaard@gmail.com>
2024-08-25 16:31:45 +0200

    Changed everything into a more personal approach.

--- README.md
+++ README.md
@@ -1,39 +1,54 @@
-# ubuntu-debullshit!
+# Gnomify Desktop!
   
-#### An automated script to set-up Ubuntu as it should be. Tested on Ubuntu 22.04 and 23.04
+## Overview 
+### A script to change Ubuntu into a clean Gnome experince.
+This script is a fork of [ubuntu-debullshit!]() and is intended to be used on a fresh install of Ubuntu with no additional software.
+It will remove the Ubuntu desktop environment and install the near vanilla Gnome desktop environment with some additional tweaks.
 
-<img src="https://raw.githubusercontent.com/polkaulfield/ubuntu-debullshit/main/menu.png" width="500" />
-  
-#### Features:
+The script will also install many of the official Gnome applications.
+
+### Who is this for?
+This script is for people who want a clean Gnome experience on Ubuntu.
+But still want the ease of use that Ubuntu provides with its distribution.
+
+### Screenshot of the menu
+<img src="https://raw.githubusercontent.com/SirBisgaard/Gnomify-Distro/main/menu.png" width="500" />
+
+I made it a one option script to make it easier for people to use, but if you want to see what the script does, you can read the script and run the commands manually.
+
+## Preview
+
+Here is a screenshot of the desktop after running the script, with the default wallpaper and the Papirus icon theme.
+
+<img src="https://raw.githubusercontent.com/SirBisgaard/Gnomify-Distro/main/screenshot.png" width="500" />
+
+
+## Installation
+I recommend a minimal install of the latest version of Ubuntu. 
+To use the script, open a terminal and run the following command:
+
+**Use this script at your own risk.**
+
+`sudo bash -c "$(wget -qO- https://raw.githubusercontent.com/SirBisgaard/Gnomify-Distro/main/gnomify-distro.sh)"`
+
+
+After the the script has finished, you will need to reboot your computer.
+
+## Features
+The script does the following:
 
 * Removes snaps completely
-* Installs a vanilla gnome session
-* Sets up flathub and gnome-software with the flatpak plugin
-* Installs gnome-tweaks
-* Installs [Extension Manager](https://github.com/mjakeman/extension-manager)
-* Disables the Ubuntu theming
-* Enables the libadwaita theme in gtk3 apps using [adw-gtk3](https://github.com/lassekongo83/adw-gtk3)
-* Installs the adwaita 46 icon theme and morewaita for extended icon support
-* Enables gnome integration with QT apps
-* Installs Firefox from the Mozilla Repository
-* Disables the data reporting component
-* Disables the annoying crash popup
 * Removes terminal ads
-
-TL;DR, you will end up with a clean GNOME desktop with flatpaks, similar to a fresh Fedora install.
-
-#### Preview
-
-<img src="https://raw.githubusercontent.com/polkaulfield/ubuntu-debullshit/main/screenshot.png" width="500" />
-
-#### Installation
-
-I recommend a minimal install of Ubuntu 23.04 to run this. 
-
-You can download the script from the repo or use this oneliner command, there are no dependencies required.
-
-`sudo bash -c "$(wget -qO- https://raw.githubusercontent.com/polkaulfield/ubuntu-debullshit/main/ubuntu-debullshit.sh)"`
-
-After the install, reboot and enjoy a clean experience :)
-
-<a href='https://ko-fi.com/polkaulfield' target='_blank'><img height='35' style='border:0px;height:46px;' src='https://az743702.vo.msecnd.net/cdn/kofi3.png?v=0' border='0' alt='Buy Me a Coffee at ko-fi.com' />
+* Removes the data reporting component
+* Removes the application crash popup
+* Removes the Ubuntu desktop environment
+* Installs vanilla Gnome desktop environment
+* Installs gnome-software-center, gnome-tweaks, gnome-backgrounds
+* installs flatpak and gnome-software flatpak plugin
+* Installs [Extension Manager](https://github.com/mjakeman/extension-manager)
+* Installs Firefox from the Mozilla Repository
+* Installs [Papirus icon theme](https://github.com/PapirusDevelopmentTeam/papirus-icon-theme) and enables it
+* Installs most of the official Gnome applications (See script for details)
+* Enables gnome integration with QT apps
+* Enables the libadwaita theme in gtk3 apps using [adw-gtk3](https://github.com/lassekongo83/adw-gtk3)
+* Enables flathub repository
---
+++ gomify-distro.sh
@@ -0,0 +1,231 @@
+#!/usr/bin/env bash
+
+disable_ubuntu_report() {
+    ubuntu-report send no
+    apt remove ubuntu-report -y
+}
+
+remove_appcrash_popup() {
+    apt remove apport apport-gtk -y
+}
+
+remove_snaps() {
+    while [ "$(snap list | wc -l)" -gt 0 ]; do
+        for snap in $(snap list | tail -n +2 | cut -d ' ' -f 1); do
+            snap remove --purge "$snap"
+        done
+    done
+
+    systemctl stop snapd
+    systemctl disable snapd
+    systemctl mask snapd
+    apt purge snapd -y
+    rm -rf /snap /var/snap /var/lib/snapd
+    for userpath in /home/*; do
+        rm -rf $userpath/snap
+    done
+    cat <<-EOF | tee /etc/apt/preferences.d/nosnap.pref
+	Package: snapd
+	Pin: release a=*
+	Pin-Priority: -10
+	EOF
+}
+
+disable_terminal_ads() {
+    sed -i 's/ENABLED=1/ENABLED=0/g' /etc/default/motd-news
+    pro config set apt_news=false
+}
+
+update_system() {
+    apt update & apt upgrade --auto-remove -y
+}
+
+cleanup() {
+    apt autoremove -y
+}
+
+setup_flathub() {
+    apt install flatpak -y
+    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    apt install --no-install-recommends gnome-software gnome-software-plugin-flatpak -y
+}
+
+setup_vanilla_gnome() {
+    apt install qgnomeplatform-qt5 -y
+    apt install gnome-session fonts-cantarell papirus-icon-theme gnome-backgrounds gnome-shell-extension-manager gnome-tweaks vanilla-gnome-default-settings gnome-shell-extension-manager vanilla-gnome-desktop -y 
+    apt remove ubuntu-session yaru-theme-gnome-shell yaru-theme-gtk yaru-theme-icon yaru-theme-sound -y
+    
+	gsettings_wrapper set org.gnome.desktop.interface monospace-font-name "Monospace 10"
+    gsettings_wrapper set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/gnome/blobs-l.svg'
+    gsettings_wrapper set org.gnome.desktop.background picture-uri-dark 'file:///usr/share/backgrounds/gnome/blobs-l.svg'
+}
+
+setup_gnome_apps() {
+    # Please create a PR with missing gnome apps
+    flatpak install flathub org.gnome.Calculator -y
+    flatpak install flathub org.gnome.Calculator -y
+    flatpak install flathub org.gnome.Calendar -y
+    flatpak install flathub org.gnome.Contacts -y
+    flatpak install flathub org.gnome.Epiphany -y
+    flatpak install flathub org.gnome.Loupe -y
+    flatpak install flathub org.gnome.Music -y
+    flatpak install flathub org.gnome.Papers -y
+    flatpak install flathub org.gnome.Photos -y
+    flatpak install flathub org.gnome.Showtime -y
+    flatpak install flathub org.gnome.Snapshot -y
+    flatpak install flathub org.gnome.Weather -y
+    flatpak install flathub org.gnome.Maps -y
+    flatpak install flathub org.gnome.seahorse.Application -y
+}
+
+install_adwgtk3() {    
+    apt install adw-gtk3 -y
+    if command -v flatpak; then
+        flatpak install -y runtime/org.gtk.Gtk3theme.adw-gtk3-dark
+        flatpak install -y runtime/org.gtk.Gtk3theme.adw-gtk3
+    fi
+    if [ "$(gsettings_wrapper get org.gnome.desktop.interface color-scheme | tail -n 1)" == ''\''prefer-dark'\''' ]; then
+        gsettings_wrapper set org.gnome.desktop.interface gtk-theme adw-gtk3-dark
+        gsettings_wrapper set org.gnome.desktop.interface color-scheme prefer-dark
+	    gsettings_wrapper set org.gnome.desktop.interface icon-theme Papirus
+    else
+        gsettings_wrapper set org.gnome.desktop.interface gtk-theme adw-gtk3
+	    gsettings_wrapper set org.gnome.desktop.interface icon-theme Papirus
+    fi
+}
+
+install_icons() {
+    apt install adwaita-icon-theme -y 
+    # Source: https://github.com/PapirusDevelopmentTeam/papirus-icon-theme
+    add-apt-repository ppa:papirus/papirus
+    apt-get update
+    apt-get install papirus-icon-theme -y  # Papirus, Papirus-Dark, and Papirus-Light
+}
+
+restore_firefox() {
+    apt purge firefox -y
+    snap remove --purge firefox
+    wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- > /etc/apt/keyrings/packages.mozilla.org.asc
+    echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" > /etc/apt/sources.list.d/mozilla.list 
+    echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+' > /etc/apt/preferences.d/mozilla
+    apt update
+    apt install firefox -y
+}
+
+gsettings_wrapper() {
+    if ! command -v dbus-launch; then
+        sudo apt install dbus-x11 -y
+    fi
+    sudo -Hu $(logname) dbus-launch gsettings "$@"
+}
+
+ask_reboot() {
+    echo 'Reboot now? (y/n)'
+    while true; do
+        read choice
+        if [[ "$choice" == 'y' || "$choice" == 'Y' ]]; then
+            reboot
+            exit 0
+        fi
+        if [[ "$choice" == 'n' || "$choice" == 'N' ]]; then
+            break
+        fi
+    done
+}
+
+msg() {
+    tput setaf 2
+    echo "[*] $1"
+    tput sgr0
+}
+
+error_msg() {
+    tput setaf 1
+    echo "[!] $1"
+    tput sgr0
+}
+
+check_root_user() {
+    if [ "$(id -u)" != 0 ]; then
+        echo 'Please run the script as root!'
+        echo 'We need to do administrative tasks'
+        exit
+    fi
+}
+
+print_banner() {
+    echo '
+  ██████  ███    ██  ██████  ███    ███ ██ ███████ ██    ██     ██████  ██ ███████ ████████ ██████   ██████  
+ ██       ████   ██ ██    ██ ████  ████ ██ ██       ██  ██      ██   ██ ██ ██         ██    ██   ██ ██    ██ 
+ ██   ███ ██ ██  ██ ██    ██ ██ ████ ██ ██ █████     ████       ██   ██ ██ ███████    ██    ██████  ██    ██ 
+ ██    ██ ██  ██ ██ ██    ██ ██  ██  ██ ██ ██         ██        ██   ██ ██      ██    ██    ██   ██ ██    ██ 
+  ██████  ██   ████  ██████  ██      ██ ██ ██         ██        ██████  ██ ███████    ██    ██   ██  ██████  
+                                                                                                            
+ By: @SirBisgaard | Forked from: @polkaulfield               
+    '                                                                                                       
+}
+
+show_menu() {
+    echo ' Menu: '
+    echo '   1 - Gnomify Distro'
+    echo '   q - Exit'
+    echo
+}
+
+main() {
+    check_root_user
+    while true; do
+        print_banner
+        show_menu
+        read -p 'Enter your choice: ' choice
+        case $choice in
+        1)
+            auto
+            msg 'Done!'
+            ask_reboot
+            ;;
+    
+        q)
+            exit 0
+            ;;
+
+        *)
+            error_msg 'Wrong input!'
+            ;;
+        esac
+    done
+
+}
+
+auto() {
+    msg 'Updating system'
+    update_system
+    msg 'Removing Ubuntu report'
+    disable_ubuntu_report
+    msg 'Removing appcrash popup'
+    remove_appcrash_popup
+    msg 'Removing terminal ads (if they are enabled)'
+    disable_terminal_ads
+    msg 'Installing Firefox from mozilla repository'
+    restore_firefox
+    msg 'Installing vanilla icons and Papirus icons'
+    install_icons
+    msg 'Installing vanilla Gnome session'
+    setup_vanilla_gnome
+    msg 'Install adw-gtk3 and set dark theme'
+    install_adwgtk3
+    msg 'Removing snap'
+    remove_snaps
+    msg 'SettInstalling flatpak and flathub'
+    setup_flathub
+    msg 'Installing Gnome apps from flathub'
+    setup_gnome_apps
+    msg 'Cleaning up'
+    cleanup
+}
+
+(return 2> /dev/null) || main
